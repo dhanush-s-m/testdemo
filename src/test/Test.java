dbc9a06c7ab5165fc8e3eaa880eeccde802dd897
dhanush-s-m <54267530+dhanush-s-m@users.noreply.github.com>
2021-07-30 10:50:12 +0530

    Update Test.java
    saved

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,8 +2,8 @@ package test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("hi");
-		System.out.println("hello");
+		
+		System.out.println("hi hello");
 	}
 
 }
